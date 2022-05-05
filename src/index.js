bd596c4c2941e2cb5e85654d9c96f063f4670cc6
Simeon2001 <jesusanyasimeon@gmail.com>
2022-05-05 11:36:26 +0100

    learning about get request

--- src/index.js
+++ src/index.js
@@ -3,4 +3,17 @@ const express = require("express");
 const app = express();
 const PORT  =   8000;
 
-app.listen(PORT, () => console.log("running express server on port ${PORT}"))
+app.listen(PORT, () => console.log("running express server on port ${PORT} "));
+
+app.get('/list',(request,response) => {
+    response.send([
+        {
+            item:"milk",
+            quantity:44
+        },
+        {
+            item: "soup",
+            quantity: 4
+        }
+    ]);
+});
